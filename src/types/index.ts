6acfa442f6a34006f9f05fae8c36e48760fbaf00
Joost Lubach <joostlubach@gmail.com>
2025-09-14 11:01:21 +0200

    Extended params

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,13 @@
 import { Override, Transition } from '../specification'
 import { Block } from './pug'
 
+export * from './params'
 export * from './pug'
 
 export interface TemplateConfig {
   fps:    number
   width:  number
   height: number
-}
-
-export interface TemplateParameter<T = any> {
-  name:         string
-  caption:      string
-  type:         TemplateVariableType
-  visible?:     boolean
-  min?:         number
-  max?:         number
-  optional?:    boolean
-  yes_caption?: string
-  no_caption?:  string
-  default?:     T
-}
-
-export enum TemplateVariableType {
-  Image = 'image',
-  Text = 'text',
-  Multiline = 'multiline',
-  Number = 'number',
-  Boolean = 'boolean',
-  Outlet = 'outlet',
-  Object = 'object'
 }
 
 export type TemplateStructure = Block
