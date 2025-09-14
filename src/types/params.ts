dead99b7f0d8c96e24e015270705891d4bd2cf63
Joost Lubach <joostlubach@gmail.com>
2025-09-14 11:01:21 +0200

    Extended params

---
+++ src/types/params.ts
@@ -0,0 +1,57 @@
+
+
+export * from './pug'
+
+export type TemplateParameter =
+  | TextParameter
+  | ImageParameter
+  | NumberParameter
+  | BooleanParameter
+  | SelectParameter
+  | ObjectParameter
+
+export interface TemplateParameterCommon<T> {
+  name:     string
+  caption:  string
+  optional: boolean
+  default?: T
+}
+
+export interface TextParameter extends TemplateParameterCommon<string> {
+  type: 'text'
+  min_length?: number
+  max_length?: number
+}
+
+export interface ImageParameter extends TemplateParameterCommon<string> {
+  type: 'image'
+}
+
+export interface NumberParameter extends TemplateParameterCommon<string> {
+  type: 'number'
+  int: boolean
+  min?: number
+  max?: number
+}
+
+export interface BooleanParameter extends TemplateParameterCommon<string> {
+  type: 'boolean'
+  yes_caption?: string
+  no_caption?: string
+}
+
+export interface SelectParameter extends TemplateParameterCommon<string> {
+  type: 'select'
+  choices: SelectParameterChoice[] | WellKnownSelectParameterChoices
+}
+
+export type SelectParameterChoice = string | {value: string | number, label: string}
+export enum WellKnownSelectParameterChoices {
+  Outlet = '$outlet'
+}
+
+export interface ObjectParameter extends TemplateParameterCommon<string> {
+  type: 'object'
+  fields: TemplateParameter[]
+}
+
