7ca2ab814558fbfc6ab6e0d2881e1d1ec0cbdd83
Joost Lubach <joostlubach@gmail.com>
2025-12-15 12:28:31 +0100

    Lint

--- src/TemplateEvaluator.ts
+++ src/TemplateEvaluator.ts
@@ -1,8 +1,9 @@
 import { isArray, mapValues } from 'lodash'
 import { Attribute } from 'templates'
 import { isFunction, isPlainObject } from 'ytil'
+
 import { blacklist, global, jsep } from './jsep'
-import { ComponentSpec } from './specification'
+import { ComponentSpec, EffectSpec } from './specification'
 
 // Type declarations for jsep plugin types
 interface ObjectExpression extends jsep.Expression {
@@ -371,6 +372,8 @@ export class TemplateEvaluator {
 type evaluate<T> = (
     T extends infer C extends ComponentSpec ? Evaluated<C> | undefined :
     T extends Array<infer C extends ComponentSpec> ? Array<Evaluated<C> | undefined> :
+    T extends infer E extends EffectSpec ? Evaluated<E> | undefined :
+    T extends Array<infer E extends EffectSpec> ? Array<Evaluated<E> | undefined> :
     T extends Attribute<infer R> ? R :
     T extends Array<infer I> ? Array<Evaluated<I>> :
     evaluate<T>
